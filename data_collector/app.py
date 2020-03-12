e440a29ca395ed72148dfb2ad40376920306761c
imflikk <flikkme@gmail.com>
2020-03-12 13:26:17 -0700

    Finished real-estate scraper and started new flask app data_collector

---
+++ data_collector/app.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return render_template("index.html")
+
+@app.route("/success", methods=['POST'])
+def success():
+    if request.method == "POST":
+        email = request.form['email_value']
+        height = request.form['height_value']
+        print(email, height)
+        return render_template("success.html")
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
--- web_scraping/real_estate_scraper.py
+++ web_scraping/real_estate_scraper.py
@@ -6,9 +6,11 @@ from bs4 import BeautifulSoup
 
 
 # Updated URL: r = requests.get("http://www.pyclass.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
-# Century21 URL: https://www.century21.com/real-estate/roseville-ca/LCCAROSEVILLE/
 
-r = requests.get("https://www.century21.com/real-estate/roseville-ca/LCCAROSEVILLE/")
+base_url = "https://www.century21.com"
+
+# Search site above for appropriate URL, the one below will not load correctly.
+r = requests.get("https://www.century21.com/real-estate/city-stateabbreviation/")
 content = r.content
 
 soup = BeautifulSoup(content, "html.parser")
@@ -16,22 +18,57 @@ soup = BeautifulSoup(content, "html.parser")
 results = soup.find_all('div', {'class':'results-label'})
 total_listings = results[0].find('strong').text.replace('(', '').replace(')', '')
 
-print("Total listings in Roseville, CA: " + total_listings + "\n")
+print("Total listings: " + total_listings + "\n")
 
-properties = soup.find_all('div',{'class':'property-card'})
+properties = soup.find_all('div',{'class':'property-card'}, limit=None)
+properties_list = []
 
 for item in properties:
-    city = properties[properties.index(item)].find('div',{'class':'property-city'}).text.strip()
-    address = properties[properties.index(item)].find('div',{'class':'property-address'}).text.strip()
-    beds = properties[properties.index(item)].find('div',{'class':'property-beds'}).text.strip()
-    baths = properties[properties.index(item)].find('div',{'class':'property-baths'}).text.strip()
-    sqfeet = properties[properties.index(item)].find('div',{'class':'property-sqft'}).text.strip()
-    price = properties[properties.index(item)].find('a',{'class':'listing-price'}).text.strip()
+    d = {}
+    try:
+        d['Price'] = properties[properties.index(item)].find('a',{'class':'listing-price'}).text.strip()
+    except:
+        d['Price'] = None
+    try:
+        d['Beds'] = properties[properties.index(item)].find('div',{'class':'property-beds'}).text.strip()
+    except:
+        d['Beds'] = None
+    try:
+        d['Baths'] = properties[properties.index(item)].find('div',{'class':'property-baths'}).text.strip()
+    except:
+        d['Baths'] = None
+    try:
+        d['SqFeet'] = properties[properties.index(item)].find('div',{'class':'property-sqft'}).text.strip()
+    except:
+        d['SqFeet'] = None
+    try:
+        d['Address'] = properties[properties.index(item)].find('div',{'class':'property-address'}).text.strip()
+    except:
+        d['Address'] = None
+    try:
+        d['City'] = properties[properties.index(item)].find('div',{'class':'property-city'}).text.strip()
+    except:
+        d['City'] = None
+    try:
+        d['URL'] = base_url + properties[properties.index(item)].find('a',{'class':'listing-price'})['href']
+    except:
+        d['URL'] = None
+    
+    
+    
+    
+    
+    
 
-    print(price)
-    print(beds)
-    print(baths)
-    print(sqfeet)
-    print(address)
-    print(city)
-    print()
+    # print(price)
+    # print(beds)
+    # print(baths)
+    # print(sqfeet)
+    # print(address)
+    # print(city)
+    # print(base_url + link)
+    # print()
+    properties_list.append(d)
+
+df = pandas.DataFrame(properties_list)
+df.to_csv("output.csv")
